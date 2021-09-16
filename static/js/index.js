957a8fcd7aa8d6ca9ce78e48e00e05fafb467eab
Edopad <mgrant8164@gmail.com>
2021-09-16 13:43:08 -0700

    Parallax + Styling
    Added parallax effect and improved styling

--- static/js/index.js
+++ static/js/index.js
@@ -1,38 +1,20 @@
-function docresized() {
-  /*console.log("Window resized!");
-  rsbg();*/
-}
-/*
-var BGAR = 10173 / 1080; //Background aspect ratio (width / height)
-/*Resize Background*
-function rsbg() {
-  var bge = document.getElementById('wpbg'); //bg element
-  var bgse = document.getElementById('wpbgs'); //bg section element
-  var comw = bgse.clientHeight * BGAR; //picture width
-  bge.style.width = comw + 'px';
-  bgse.style.width = (comw + document.body.clientWidth) + 'px';
-  console.log("background resized.");
-  return true;
-}
-/*Initialize background*
-function initwpbg() {
-  rsbg();
-}
-/*Pause background*
-function pwpbg() {
-  var bge = document.getElementById('wpbg'); //bg element
-  var bgpe = document.getElementById('wpbgpb'); //pause button
-  if(bge.className !== "paused") {
-    bge.className = "paused";
-    bgpe.innerHTML = "Resume Background<br/>Scrolling";
-  } else {
-    bge.className = "";
-    bgpe.innerHTML = "Pause Background<br/>Scrolling";
-  }
+function update_parallax() {
+  var bge = document.getElementById('top_pane_bg');
+  var scrolly = window.scrollY;
+  bge.style.top = ''+scrolly/2+'px';
+  /*
+  var pce = document.getElementById('headshot');
+  pce.style.marginTop = 'calc(-' + scrolly/4 + 'px - 11em)';*/
+  window.requestAnimationFrame(update_parallax);
 }
 
-/*Util functions*
+function page_load() {
+  window.requestAnimationFrame(update_parallax);
+}
+
+/*Util functions*/
+/*
 function hasClass(element, cls) {
     return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
 }
-//*/
+*/
