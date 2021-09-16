6c58ef78421143398f16356161c37089d1de8016
Edopad <mgrant8164@gmail.com>
2021-09-16 14:17:14 -0700

    .

--- static/js/index.js
+++ static/js/index.js
@@ -1,16 +1,18 @@
 function update_parallax() {
   var bge = document.getElementById('top_pane_bg');
   var scrolly = window.scrollY;
-  bge.style.top = ''+scrolly/2+'px';
+  //bge.style.top = ''+scrolly/2+'px';
+
   /*
   var pce = document.getElementById('headshot');
   pce.style.marginTop = 'calc(-' + scrolly/4 + 'px - 11em)';*/
+  bge.style.transform = 'translateY(-'+scrolly/2+'px)';
   window.requestAnimationFrame(update_parallax);
 }
 
 function page_load() {
   window.requestAnimationFrame(update_parallax);
-  window.setInterval(update_parallax, 30);
+  //window.setInterval(update_parallax, 1000/60);
 }
 
 /*Util functions*/
